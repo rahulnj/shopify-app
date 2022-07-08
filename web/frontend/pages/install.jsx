5a456c71c967241f8513e2545ac0c53337cb4a53
rahulnj~ <rahuljayaraman8@gmail.com>
2022-07-08 10:59:49 +0530

    added install page.

--- web/frontend/pages/install.jsx
+++ web/frontend/pages/install.jsx
@@ -1,7 +1,32 @@
-import { Page } from '@shopify/polaris'
+import { Layout, Page, SettingToggle, TextStyle } from '@shopify/polaris'
+import { useState } from 'react'
 
 const install = () => {
-  return <Page>install</Page>
+  const [isInstalled, setIsInstalled] = useState(null)
+  const titleDescription = isInstalled ? 'Uninstall' : 'Install'
+  const bodyDescription = isInstalled ? 'Installed' : 'Uninstalled'
+  const handleAction = () => {
+    setIsInstalled((value) => !value)
+  }
+  return (
+    <Page>
+      <Layout.AnnotatedSection
+        title={`${titleDescription} App`}
+        description='Toggle banner installation on your shop'
+      >
+        <SettingToggle
+          action={{
+            content: titleDescription,
+            onAction: handleAction,
+          }}
+          enabled={true}
+        >
+          The banner is{' '}
+          <TextStyle variation='strong'>{bodyDescription}</TextStyle>
+        </SettingToggle>
+      </Layout.AnnotatedSection>
+    </Page>
+  )
 }
 
 export default install
